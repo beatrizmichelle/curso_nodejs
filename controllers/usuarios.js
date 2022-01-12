6eb94219682dee49262e3b29e8b73e83a59f057b
Beatriz Michelle Valencia Lavalle <chiquibetina@gmail.com>
2022-01-11 22:53:14 -0600

    Ajuste en la variable de usuario de la BD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,17 +1,76 @@
 const {request, response} = require ("express");
+const pool = require("../db/conexion");
+const usuariosQueries = require("../models/usuarios");
 
-const usuariosGet = (req = request, res = response) =>{
+
+const usuariosGet =async (req = request, res = response) =>{
+let conn;
+
+try{
+    conn = await pool.getConnection();
+
+    const usuarios = await conn.query(usuariosQueries.selectUsuarios)
+
+    res.json({ usuarios });
+} catch(error){
+    console.log(error);
+    res.status(500).json({msg: "Por favor contacte al administrador.", error});
+}finally{
+    if(conn) conn.end();
+}
+
     res.json ({ msg: "Hola a todos desde GET"});
 };
 
-const usuariosPost = (req = request, res = response) =>{
-    const {nombre, apellido, email, edad=0} = req.body;
-    res.status(201).json({ msg: "Hola a todos desde POST", edad});
+const usuariosPost = async (req = request, res = response) =>{
+    const {nombre, email, password, status = 1} = req.body;
+   
+    let conn;
+
+try{
+    conn = await pool.getConnection();
+
+    const usuarios = await conn.query(usuariosQueries.insertUsuario,[
+        nombre, 
+        email, 
+        password, 
+        status,]);
+
+    res.json({ usuarios });
+} catch(error){
+    console.log(error);
+    res.status(500).json({msg: "Por favor contacte al administrador.", error});
+}finally{
+    if(conn) conn.end();
+}
+
+    res.json ({ msg: "Hola a todos desde GET"});
+};
 };
 
-const usuariosPut = (req = request, res = response) => {
-  const {id} = req.params;
-  res.status(400).json({ msg: "Hola a todos desde PUT", id});
+const usuariosPut = async (req = request, res = response) => {
+  const {email} = req.query;
+  const {nombre, status} = req.body;
+
+  let conn;
+
+  try{
+      conn = await pool.getConnection();
+  
+      const usuarios = await conn.query(usuariosQueries.updateUsuario,[
+          nombre, 
+          status, 
+          email,]);
+  
+      res.json({ usuarios });
+  } catch(error){
+      console.log(error);
+      res.status(500).json({msg: "Por favor contacte al administrador.", error});
+  }finally{
+      if(conn) conn.end();
+  }
+  
+  
 };
 
 
---
+++ models/usuarios.js
@@ -0,0 +1,32 @@
+const usuariosQueries ={
+insertUsuario:`
+  INSERT INTO
+    usuarios(
+        nombre,
+        email,
+        password,
+        status
+    )
+  VALUES
+    (?, ?, ?, ?)
+`,
+selectUsuarios:`
+ SELECT
+  *
+ FROM
+   usuarios
+ WHERE
+   status = 1
+`,
+updateUsuario:`
+ UPDATE
+   usuarios
+ SET
+   nombre=?
+   status=?
+ WHERE
+   email=?
+`
+}
+
+module.exports = usuariosQueries;
